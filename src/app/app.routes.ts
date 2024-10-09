0d2e1df85bcd3249b1e39a2e8097cab15c425d29
kevinstring <kevingabrielmdn@gmail.com>
2024-10-09 15:11:58 -0600

    tailwind y principal
    Agregue framework taiwind y cree un componente principal de muestra

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,8 @@
 import { Routes } from '@angular/router';
+import { PrincipalComponent } from './principal/principal.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+    {
+        path:'',component:PrincipalComponent
+    }
+];
